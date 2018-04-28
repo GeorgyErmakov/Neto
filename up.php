b59ed5ef4c875004881a73ed54712428a601cedb
GeorgyErmakov <38668197+GeorgyErmakov@users.noreply.github.com>
2018-04-28 14:57:28 +0300

    Update up.php

--- up.php
+++ up.php
@@ -1,22 +1,20 @@
 <?php
 session_start();
 require 'functions.php';
-    if(isset($_FILES)) 
-      { 
-        if($_SESSION['user']['role']=='admin'){
 
-        $destiation_dir = $_SERVER['DOCUMENT_ROOT']; 
-        if (move_uploaded_file($_FILES['filename']['tmp_name'], $destiation_dir.'/'.$_FILES['filename']['name']))
-           { 
+if(isset($_FILES)) { 
+    if($_SESSION['user']['role']=='admin') {
+        if (move_uploaded_file($_FILES['filename']['tmp_name'],'./'.$_FILES['filename']['name'])) { 
             echo 'File Uploaded'; 
-           }
-           else
-           {
+        }
+        else {
             echo 'No File Uploaded'; // Оповещаем пользователя о том, что файл не был загружен
-            }
-    addTest($destiation_dir.'/'.$_FILES['filename']['name']);
-    header("Location: index.php");
-  }
-  else {header("HTTP/1.1 403 Forbidden");}
-       }
+        }
+        addTest('./'.$_FILES['filename']['name']);
+        header("Location: index.php");
+    }
+    else {
+        header("HTTP/1.1 403 Forbidden");
+    }
+}
 ?>
