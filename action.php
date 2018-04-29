650c4779357e5e623efe8dec5e5a964027a50b33
GeorgyErmakov <38668197+GeorgyErmakov@users.noreply.github.com>
2018-04-29 23:39:10 +0300

    Update action.php

--- action.php
+++ action.php
@@ -5,13 +5,11 @@
 
     if ($_POST['testgo']=="testgo"){
     	header("Location: test.php"."?test_id=".$_POST['testid']);
-    	echo "string";
     }
     
     if ($_POST['testdel']=="testdel"){
         delTest($_POST['testid']);
         header("Location: index.php");
-        echo "string";
     }
     
     if ($_POST['logout']=="logout"){
