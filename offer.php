20a3d72f0faac98f30c10dc37beefff622df59d1
GeorgyErmakov <38668197+GeorgyErmakov@users.noreply.github.com>
2018-05-02 23:59:31 +0300

    Create offer.php

---
+++ offer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace offer
+
+interfaces OfferInterfaces
+{
+    public function getOffer($basket);
+	public function approveOffer();
+    public function cancelOffer();
+	public function printOffer();
+}
+
+class OfferClass implements OfferInterfaces
+{
+	private $offerid;
+	private $basket;
+	public $buyer;
+	public $delivery;
+
+	public function getOffer($basket);
+	public function approveOffer();
+    public function cancelOffer();
+	public function printOffer();
+}
+
+?>
