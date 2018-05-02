a38b91a32f0bfd7f6b0039100db2b54df000f725
GeorgyErmakov <38668197+GeorgyErmakov@users.noreply.github.com>
2018-05-03 00:02:35 +0300

    Create traits.php

---
+++ traits.php
@@ -0,0 +1,25 @@
+<?php
+
+trait ExtraWeightDiscount 
+{
+	public function getDisc()
+	{
+		if ($this->weight>10)
+		{
+        return $this->discount;
+	    }
+	    else{
+	    return $this->discount=0;
+	    }
+	}
+} 
+
+trait GeneralDiscount 
+{
+	public function getPrice()
+	{
+    return $this->price*0.1;
+	}
+}
+
+?>
