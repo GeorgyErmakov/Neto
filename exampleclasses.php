e2cf523dcd63d3bb314d1f1cc21f755a895d11d8
GeorgyErmakov <38668197+GeorgyErmakov@users.noreply.github.com>
2018-05-04 00:32:50 +0300

    Update exampleclasses.php

--- exampleclasses.php
+++ exampleclasses.php
@@ -22,17 +22,17 @@ class CarClass
 	
     public function carStartEngine() //метод включения зажигания автомобиля
     {
-	return true;
+    return true;
     } 
 	
     public function carAccelerate($carDrive, $carAcc) //метод ускорения машины
     {
-	return $speed;
+    return $speed;
     }
 	
     public function carBreak($carSpeed) //метод остановки машины
     {
-	return true;
+    return true;
     }
 }
 
@@ -44,26 +44,25 @@ class TvClass
 	
     public function __construct($model, $type, $vendor)
     {
-	$this->tvtype = $type;
-	$this->tvVendor = $vendor;
-	$this->tvModel = $model;
-	
-	return true;
+    $this->tvtype = $type;
+    $this->tvVendor = $vendor;
+    $this->tvModel = $model;	
+    return true;
     } 
 
     public function tvOn() // функция включения ТВ
     {
-        return true;
+    return true;
     } 
     
     public function tvOff() //функция выключения ТВ
     {
-	return true;
+    return true;
     } 
 
     public function tvSwitch($channal) //функция переключения телевизора на нужный канал $channal
     {
-	return true;
+    return true;
     }
 }
 
@@ -75,26 +74,25 @@ class PencilClass
 
     public function __construct($color, $type, $vendor)
     {
-	$this->PencilType = $type;
-	$this->PencilVendor = $vendor;
-	$this->PencilColor = $color;
-	
-	return true;
+    $this->PencilType = $type;
+    $this->PencilVendor = $vendor;
+    $this->PencilColor = $color;	
+    return true;
     } 
 
     public function PencilOpen() // метод открытия ручки
     {
-	return true;
+    return true;
     } 
    
     public function PencilClose() // метод закрытия ручки
     {
-	return true;
+    return true;
     } 
 
     public function PencilFill($vol) //метод запарвки чернил на объем $vol
     {
-	return true;
+    return true;
     }
 }
 
@@ -107,32 +105,31 @@ class DuckClass
 
     public function __construct($gender, $old, $type, $name)
     {
-	$this->DuckGender = $gender;
-	$this->DuckOld = $old;
-	$this->DuckType = $type;
-	$this->DuckName = $name;
-	
-	return true;
+    $this->DuckGender = $gender;
+    $this->DuckOld = $old;
+    $this->DuckType = $type;
+    $this->DuckName = $name;
+    return true;
     } 
 
     public function duckEat() // метод поглощения пищи, 
     {
-	return true;
+    return true;
     } 
 
     public function duckRun($speed) // метод ходьбы и полета, приводит объект в движение на скорости $speed
     {
-	return true;
+    return true;
     } 
 
     public function duckFly() //метод взлета утки
     {
-	return true;
+    return true;
     }
 
     public function duckSleep() //метод сна утки
     {
-	return true;
+    return true;
     }
 }
 
@@ -148,28 +145,27 @@ class GoodsClass
     public function __construct($vendor, $name, $price, $desc, $disc, $pic)
     {
     $this->goodVendor = $vendor;
-	$this->goodName = $name;
-	$this->goodPrice = $price;
-	$this->goodDesc = $desc;
-	$this->goodDiscount = $disc;
-	$this->goodPic = $pic;
-	
-	return true;
+    $this->goodName = $name;
+    $this->goodPrice = $price;
+    $this->goodDesc = $desc;
+    $this->goodDiscount = $disc;
+    $this->goodPic = $pic;	
+    return true;
     } 
 
     public function goodGetPrice() // метод получения цены
     {
-	return true;
+    return true;
     } 
 	
     public function goodBuy($bool) // метод добавления в корзину или удаления из нее
     {
-	return true;
+    return true;
     } 
 
     public function goodCompare() // метод добавления к сравнению
     {
-	return true;
+    return true;
     } 
 }
 
