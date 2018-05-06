310bc88660d563d938fbd0594062ef6d38f86e25
GeorgyErmakov <38668197+GeorgyErmakov@users.noreply.github.com>
2018-05-06 12:10:17 +0300

    Update news.php

--- news.php
+++ news.php
@@ -8,43 +8,44 @@ class NewsClass
    
     public function __construct($title, $date, $content)
     {
-    $this->newsTitle=$title;
-    $this->newsDate=$date;
-    $this->newsContent=$content;
-    $this->newsComments=array();
-    return true;
+        $this->newsTitle=$title;
+        $this->newsDate=$date;
+        $this->newsContent=$content;
+        $this->newsComments=array();
+        return true;
     } 
    
     public function postNews()
     {
-    echo $this->newsDate.'<h2>'.$this->newsTitle.'</h2><p>'.$this->newsContent.'</p>';
-    return true;
+        echo $this->newsDate.'<h2>'.$this->newsTitle.'</h2><p>'.$this->newsContent.'</p>';
+        return true;
     } 
    
     public function getComments()
     {
-    return true;
+        return true;
     } 
    
     public function postComments()
     {	
-    echo newsDate;
-    return true;
+        echo newsDate;
+        return true;
     } 
 }
 
-function getNews() {
-    $json = file_get_contents('./news.json');
-    $data = json_decode($json, true);
-    return $data;
-}
-
-$newsarr = getNews();
-
-if (count($newsarr)) {
-    foreach ($newsarr as $i) {
-        $news = new NewsClass($i['title'], $i['date'], $i['content']);
-        $news->postNews();
+    function getNews() 
+    {
+        $json = file_get_contents('./news.json');
+        $data = json_decode($json, true);
+        return $data;
     }
-}
+
+    $newsarr = getNews();
+
+    if (count($newsarr)) {
+        foreach ($newsarr as $i) {
+            $news = new NewsClass($i['title'], $i['date'], $i['content']);
+            $news->postNews();
+        }
+    }
 ?>
