192f9c348c7de5487cddc035ea28f53ad40d9d46
GeorgyErmakov <38668197+GeorgyErmakov@users.noreply.github.com>
2018-05-06 12:07:52 +0300

    Update exampleclasses.php

--- exampleclasses.php
+++ exampleclasses.php
@@ -13,26 +13,26 @@ class CarClass
     
     public function __construct($model, $type, $brand, $date)
     {
-    $this->carModel = $model;
-    $this->carType = $type;
-    $this->carBrand = $brand;
-    $this->carDate = $date;
-    return true;
+        $this->carModel = $model;
+        $this->carType = $type;
+        $this->carBrand = $brand;
+        $this->carDate = $date;
+        return true;
     } 
 	
     public function carStartEngine() //метод включения зажигания автомобиля
     {
-    return true;
+        return true;
     } 
 	
     public function carAccelerate($carDrive, $carAcc) //метод ускорения машины
     {
-    return $speed;
+        return $speed;
     }
 	
     public function carBreak($carSpeed) //метод остановки машины
     {
-    return true;
+        return true;
     }
 }
 
@@ -44,25 +44,25 @@ class TvClass
 	
     public function __construct($model, $type, $vendor)
     {
-    $this->tvtype = $type;
-    $this->tvVendor = $vendor;
-    $this->tvModel = $model;	
-    return true;
+        $this->tvtype = $type;
+        $this->tvVendor = $vendor;
+        $this->tvModel = $model;	
+        return true;
     } 
 
     public function tvOn() // функция включения ТВ
     {
-    return true;
+        return true;
     } 
     
     public function tvOff() //функция выключения ТВ
     {
-    return true;
+        return true;
     } 
 
     public function tvSwitch($channal) //функция переключения телевизора на нужный канал $channal
     {
-    return true;
+        return true;
     }
 }
 
@@ -74,25 +74,25 @@ class PencilClass
 
     public function __construct($color, $type, $vendor)
     {
-    $this->PencilType = $type;
-    $this->PencilVendor = $vendor;
-    $this->PencilColor = $color;	
-    return true;
+        $this->PencilType = $type;
+        $this->PencilVendor = $vendor;
+        $this->PencilColor = $color;	
+        return true;
     } 
 
     public function PencilOpen() // метод открытия ручки
     {
-    return true;
+        return true;
     } 
    
     public function PencilClose() // метод закрытия ручки
     {
-    return true;
+        return true;
     } 
 
     public function PencilFill($vol) //метод запарвки чернил на объем $vol
     {
-    return true;
+        return true;
     }
 }
 
@@ -105,31 +105,31 @@ class DuckClass
 
     public function __construct($gender, $old, $type, $name)
     {
-    $this->DuckGender = $gender;
-    $this->DuckOld = $old;
-    $this->DuckType = $type;
-    $this->DuckName = $name;
-    return true;
+        $this->DuckGender = $gender;
+        $this->DuckOld = $old;
+        $this->DuckType = $type;
+        $this->DuckName = $name;
+        return true;
     } 
 
     public function duckEat() // метод поглощения пищи, 
     {
-    return true;
+        return true;
     } 
 
     public function duckRun($speed) // метод ходьбы и полета, приводит объект в движение на скорости $speed
     {
-    return true;
+        return true;
     } 
 
     public function duckFly() //метод взлета утки
     {
-    return true;
+        return true;
     }
 
     public function duckSleep() //метод сна утки
     {
-    return true;
+        return true;
     }
 }
 
@@ -144,34 +144,34 @@ class GoodsClass
 
     public function __construct($vendor, $name, $price, $desc, $disc, $pic)
     {
-    $this->goodVendor = $vendor;
-    $this->goodName = $name;
-    $this->goodPrice = $price;
-    $this->goodDesc = $desc;
-    $this->goodDiscount = $disc;
-    $this->goodPic = $pic;	
-    return true;
+        $this->goodVendor = $vendor;
+        $this->goodName = $name;
+        $this->goodPrice = $price;
+        $this->goodDesc = $desc;
+        $this->goodDiscount = $disc;
+        $this->goodPic = $pic;	
+        return true;
     } 
 
     public function goodGetPrice() // метод получения цены
     {
-    return true;
+        return true;
     } 
 	
     public function goodBuy($bool) // метод добавления в корзину или удаления из нее
     {
-    return true;
+        return true;
     } 
 
     public function goodCompare() // метод добавления к сравнению
     {
-    return true;
+        return true;
     } 
 }
 
-$car = new CarClass('CX-9', 'паркетник', 'Мазда', '2014');
-$tv = new TvClass('CVB897665', 'LED', 'ACER');
-$pencil = new PencilClass('Красная', 'Автомат', 'Parker');
-$duck = new DuckClass('male',2, 'китайская', 'Пекинская утка');
-$good = new GoodsClass ('Черноморец', 'Помидор красный', 50, 'Отличный помидор', 5, 'http://....pics/tomato.jpg');
+    $car = new CarClass('CX-9', 'паркетник', 'Мазда', '2014');
+    $tv = new TvClass('CVB897665', 'LED', 'ACER');
+    $pencil = new PencilClass('Красная', 'Автомат', 'Parker');
+    $duck = new DuckClass('male',2, 'китайская', 'Пекинская утка');
+    $good = new GoodsClass ('Черноморец', 'Помидор красный', 50, 'Отличный помидор', 5, 'http://....pics/tomato.jpg');
 ?>
