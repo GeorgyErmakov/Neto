57c44c810cdce55e9b0169224c0cea2af679ce63
GeorgyErmakov <38668197+GeorgyErmakov@users.noreply.github.com>
2018-05-13 23:27:40 +0300

    Update productsClass.php

--- productsClass.php
+++ productsClass.php
@@ -1,9 +1,9 @@
 <?php
 
-interfaces ProductInterfaces
+interface ProductInterfaces
 {
     public function getName();
-    public function getpPrice();
+    public function getPrice();
     public function getDesc();
     public function getPic();
     public function getDisc();
@@ -14,29 +14,37 @@ interfaces ProductInterfaces
     public function setDisc($discount);
 }
 
-
 class ProductClass implements ProductInterfaces
-
 {
     protected $name;
     protected $description;
     public $picture;
     protected $price;
-    protected $discount = 0;
-    protected $delivery = 250;
+    protected $discount;
+    protected $delivery;
     protected $weight;
 
-
-    public function getName() {
-        return $this->name;
+    public function __construct($name, $price, $description)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->description = $description;
+        $this->discount = 0;
+        $this->delivery = 250;
+    } 
+    
+    public function getName() 
+    {
+         return $this->name;
 	}
-
+    
     public function getPrice()
 	{
-        return $this->price;
+        return $this->price*(1-$this->getDisc()/100);
 	}
 
-    public function getDesc() {
+    public function getDesc() 
+    {
         return $this->description;
 	}
 
@@ -52,52 +60,55 @@ class ProductClass implements ProductInterfaces
 
     public function getDelivery()
 	{
-        if ($this->dicount <> 0) {
+        if ($this->getDisc() <> 0) {
             return $this->delivery+50;
         } else {
             return $this->delivery;
         }
 	}
-
+    
     public function getWeight()
     {
         return $this->weight;
 	}
-
-    public function setName($name) {
+    
+    public function setName($name) 
+    {
         $this->name = $name;
     }
-
+    
     public function setPrice($price)
-	{
+    {
         $this->price = $price;
     }
-
-    public function setDesc($desc) {
+    
+    public function setDesc($desc) 
+    {
         $this->description = $description;
     }
-
-    public function setPic($picture) {
-        $this->picture=$picture;
+    
+    public function setPic($picture) 
+    {
+        $this->picture = $picture;
 	}
-
+    
     public function setDisc($discount)
 	{
         $this->discount = $discount;
 	}
-
     public function setDelivery($del)
 	{
         $this->delivery = $del;
-
 	}
 	
     public function setWeight($weight)
 	{
         $this->weight = $weight;
 	}
+}
 
-trait ExtraWeightDiscount {
+trait ExtraWeightDiscount 
+{
     public function getDisc()
 	{
         if ($this->weight > 10) {
@@ -107,32 +118,54 @@ trait ExtraWeightDiscount {
         }
 	}
 } 
-trait GeneralDiscount {
+
+trait GeneralDiscount 
+{
+     public function getDisc()
+    {
+        return 10;    
+    }
+
     public function getPrice()
     {
-        return $this->price*0.1;
+        return $this->price*0.9;
 	}
 }
 
 class ToyClass extends ProductClass
 {
-    public ageForToys;
-    public $discount = 10;
+    public $ageForToys;
+
     use GeneralDiscount;
 }
-
 
 class ArtClass extends ProductClass
 {
-    public artType;
-    public $discount = 10;
+    public $artType;
+
     use GeneralDiscount;
 }
 
-
 class KidsFoodClass extends ProductClass
 {	
+    public $dateGood;
     use ExtraWeightDiscount;
 }
 
+$toy = new ToyClass('погремушка', 200, 'деревянная игрушка');
+$art = new ArtClass('кисть', 60, 'кисть колонок 200');
+$food = new KidsFoodClass('пюре', 500,'еда натуральная');
+$foodhuge = new KidsFoodClass('вагон попкорна', 5000,'еда ненатуральная');
+$food->setDisc(50);
+$foodhuge->setDisc(30);
+$food->setWeight(23);
+$foodhuge->setWeight(7);
+echo 'Цена: '.$toy->getPrice().' скидка '. $toy->getDisc().' доставка '.$toy->getDelivery().'<br>';
+echo 'Цена: '.$art->getPrice().' скидка '. $art->getDisc().' доставка '.$art->getDelivery().'<br>';
+echo 'Цена: '.$food->getPrice().' скидка '. $food->getDisc().' доставка '.$food->getDelivery().'<br>';
+echo 'Цена: '.$foodhuge->getPrice().' скидка '. $foodhuge->getDisc().' доставка '.$foodhuge->getDelivery().'<br>';
+
+$food->setWeight(5);
+
+echo 'Цена: '.$food->getPrice().' скидка '. $food->getDisc().' доставка '.$food->getDelivery().'<br>';
 ?>
