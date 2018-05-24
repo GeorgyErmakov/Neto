a208350cf1e4fcfb048dc271f764e31038cbc532
GeorgyErmakov <38668197+GeorgyErmakov@users.noreply.github.com>
2018-05-24 11:01:06 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,22 +1,22 @@
 <?php
 
-function createDB($dbname, $admin, $adminpass, $user, $userpass)
-{
+function connectPDO(){
+    return $pdo = new PDO("mysql:host=localhost;dbname=germakov;charset=UTF8", "germakov", "neto1723");
+}
 
-    $pdo = new PDO("mysql:host=localhost", $admin, $pass);
-    $createdb = "CREATE DATABASE `:db`;
-                CREATE USER ':admin'@'localhost' IDENTIFIED BY ':pass';
-                GRANT ALL ON `:db`.* TO ':admin'@'localhost';
-                FLUSH PRIVILEGES;";
-                $stmt = $pdo->prepare($createdb);
-                $stmt->execute(["db"=>$dbname, "admin"=>$user, "pass"=>$userpass]);
+function createTB($tbname)
+{
+    $pdo = connectPDO();
+    $createtb = 'CREATE TABLE '.$tbname.' ( idz int NOT NULL AUTO_INCREMENT, name varchar(50) NULL, estimation float NOT NULL, budget tinyint(4) NOT NULL DEFAULT 0, PRIMARY KEY (idz) ) ENGINE=InnoDB DEFAULT CHARSET=utf8;';
+                $stmt = $pdo->prepare($createtb);
+                $stmt->execute();
     $pdo = null;
 }
 
 
 function showDB()
 {
-    $pdo = new PDO("mysql:host=localhost", "germakov", "neto1723");
+    $pdo = connectPDO();
     $showdb = 'SHOW DATABASES;';
 
     foreach($pdo->query($showdb) as $row) {
@@ -29,8 +29,7 @@ function showDB()
 
 function showTables($dbname)
 {
-    $connect='mysql:host=localhost;dbname='.$dbname.';charset=UTF8';
-    $pdo = new PDO($connect,"germakov", "neto1723");
+    $pdo = connectPDO();
     $showtables = 'SHOW TABLES;';
     $stmt = $pdo->prepare($showtables);
     $stmt->execute();
@@ -47,21 +46,45 @@ function showTables($dbname)
 
 function descTables($dbname, $table)
 {
-    $connect='mysql:host=localhost;dbname='.$dbname.';charset=UTF8';
-    $pdo = new PDO($connect,"germakov", "neto1723");
+    $pdo = connectPDO();
     $showf = 'DESCRIBE '.$table.';';
     $stmt = $pdo->prepare($showf);
     $stmt->execute();
     $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
-    echo '<h2>Список полей и свойств таблицы  '.$dbname.'.'.$table.'</h2>';
+    echo '<h2>Список полей и свойств таблицы  '.$dbname.'.'.$table.'</h2>
+
+        <table>
+        <tr>
+        <th>row</th>
+        <th>type</th>
+        <th>Action</th>';
     foreach($res as $value) {
-        echo $value['Field'].' '.$value['Type'];
-        echo"<br />";
+        echo '<tr><form method="POST"><td><input type="hidden" name="tb" value='.$table.'><input type="hidden" name="fieldold" value='.$value['Field'].'><input name="field" value='.$value['Field'].'></td><td><input name="type" value='.$value['Type'].'></td><td> <input type="submit" name="change" value="Изменить" /><input type="submit" name="del" value="Удалить" /></td></form></tr>';
         }
+        echo '</table>';
     $pdo = null;
 }
 
 
+
+function chField($table, $name,$newname,$type)
+{
+    $pdo = connectPDO();
+    $ch = 'ALTER TABLE '.$table.' CHANGE '.$name.' '.$newname.' '. $type.';';
+    $stmt = $pdo->prepare($ch);
+    $stmt->execute();
+    $pdo = null;
+}
+
+
+function delField($table, $name)
+{
+    $pdo = connectPDO();
+    $ch = 'ALTER TABLE '.$table.' DROP COLUMN '.$name.';';
+    $stmt = $pdo->prepare($ch);
+    $stmt->execute();
+    $pdo = null;
+}
 
 ?>
